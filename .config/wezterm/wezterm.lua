a5c84156b6b4e2e5307f8e3d08caf26bbc957b4c
Jose-Salazar-27 <jose.dev2708@gmail.com>
2025-04-11 07:02:44 -0400

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,14 +11,28 @@ config.color_scheme_dirs = { "~/.config/wezterm/themes/" }
 -- config.color_scheme = "tokyonight_night"
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 
-config.font = wezterm.font("Iosevka NF")
-config.font_size = 16.0
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+config.front_end = "WebGpu"
+config.font_size = 14.0
 
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_scroll_bar = false
 
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 -- start wezterm with fish shell instead of use zsh
-config.default_prog = { "/usr/bin/fish", "-l" }
+-- config.default_prog = { "/usr/bin/fish", "-l" }
+
+-- wezterm.on('gui-startup', function(cmd)
+--     wezterm.run_command("~/.local/bin/tmux-fish.sh")
+-- end)
 
 config.enable_wayland = false
 -- and finally, return the configuration to wezterm
