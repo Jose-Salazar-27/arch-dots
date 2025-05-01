22d317edbd87cf5ecfa984e420ca6f1752050e64
Jose-Salazar-27 <jose.dev2708@gmail.com>
2025-05-01 10:21:44 -0400

    update nvim lint

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,3 +1,30 @@
+local handle_golangcilint_version = function()
+  if string.find(vim.fn.system({ "golangci-lint", "version" }), "version v2") then
+    return {
+      "run",
+      "--output.json.path=stdout",
+      "--issues-exit-code=0",
+      "--show-stats=false",
+      function()
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+      end,
+    }
+  else
+    return {
+      "run",
+      "--out-format",
+      "json",
+      "--issues-exit-code=0",
+      "--show-stats=false",
+      "--print-issued-lines=false",
+      "--print-linter-name=false",
+      function()
+        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+      end,
+    }
+  end
+end
+
 return {
   "mfussenegger/nvim-lint",
   event = {
@@ -18,16 +45,20 @@ return {
       dockerfile = { "hadolint" },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+    -- read at: https://github.com/mfussenegger/nvim-lint/issues/760#issuecomment-2758985229
+    local golang_linter = lint.linters.golangcilint
+    golang_linter.args = handle_golangcilint_version()
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged," }, {
-      group = lint_augroup,
+    -- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      -- group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
 
-    vim.keymap.set("n", "<leader>ll", function()
+    vim.keymap.set("n", "<leader>tl", function()
       lint.try_lint()
     end, { desc = "Trigger linting for current file" })
   end,
