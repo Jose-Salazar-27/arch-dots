6a02e8e2a1bcf708ee6a73b3690e345c3e6f89ba
Jose-Salazar-27 <jose.dev2708@gmail.com>
2025-08-19 11:47:41 +0000

    add better go pkg completions

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,7 +1,16 @@
 return {
   "saghen/blink.cmp",
   -- optional: provides snippets for the snippet source
-  dependencies = { "rafamadriz/friendly-snippets" },
+  dependencies = {
+    { "rafamadriz/friendly-snippets" },
+    {
+      "edte/blink-go-import.nvim",
+      ft = "go",
+      config = function()
+        require("blink-go-import").setup()
+      end,
+    },
+  },
 
   -- use a release tag to download pre-built binaries
   version = "1.*",
@@ -60,7 +69,14 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "go_pkgs" },
+
+      providers = {
+        go_pkgs = {
+          module = "blink-go-import",
+          name = "Import",
+        },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
