84a13b431ffb9315d3ffc66e621de1a059c4f9fb
Jose-Salazar-27 <jose.dev2708@gmail.com>
2025-10-29 10:35:04 +0000

    change nvim default theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,47 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
-    priority = 1000,
-    opts = {
-      -- transparent = true,
-      -- styles = {
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
-    },
-    -- config = function()
-    --   vim.cmd([[colorscheme tokyonight-night]])
-    -- end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   name = "tokyonight",
+  --   priority = 1000,
+  --   opts = {
+  --     -- transparent = true,
+  --     -- styles = {
+  --     --   sidebars = "transparent",
+  --     --   floats = "transparent",
+  --     -- },
+  --   },
+  --   config = function()
+  --     vim.cmd([[colorscheme tokyonight-night]])
+  --   end,
+  -- },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
   },
-  {
-    "rebelot/kanagawa.nvim",
-  },
-  {
-    "thesimonho/kanagawa-paper.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  -- },
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   priority = 1000,
+  -- },
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  -- },
+  -- {
+  --   "thesimonho/kanagawa-paper.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
 }
