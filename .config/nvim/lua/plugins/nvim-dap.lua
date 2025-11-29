900639c7541bb4db5d64cc6b2c4c3ae29b0b0f32
Jose-Salazar-27 <jose.dev2708@gmail.com>
2025-11-29 11:09:00 +0000

    update nvim debbuging config

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -2,113 +2,138 @@ return {
   {
     "mfussenegger/nvim-dap",
     dependencies = {
-      "nvim-neotest/nvim-nio",
       "rcarriga/nvim-dap-ui",
-      "theHamsta/nvim-dap-virtual-text",
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {}, -- Default options
+      },
+    },
+    keys = {
+      {
+        "<leader>db",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Toggle Breakpoint",
+      },
+      {
+        "<leader>dc",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<leader>dsi",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<leader>dus",
+        function()
+          local widgets = require("dap.ui.widgets")
+          local sidebar = widgets.sidebar(widgets.scopes)
+          sidebar.open()
+        end,
+        desc = "Open debugging sidebar",
+      },
+      {
+        "<leader>dS",
+        function()
+          require("dap").session()
+        end,
+        desc = "Session",
+      },
+      {
+        "<leader>dt",
+        function()
+          require("dap").terminate()
+        end,
+        desc = "Terminate",
+      },
+      {
+        "<leader>duh",
+        function()
+          require("dap.ui.widgets").hover()
+        end,
+        desc = "Do hover",
+      },
     },
     config = function()
       local dap = require("dap")
-      local dapui = require("dapui")
 
-      require("dapui").setup({})
-      require("nvim-dap-virtual-text").setup({
-        commented = true, -- Show virtual text alongside comment
-      })
-
-      vim.fn.sign_define("DapBreakpoint", {
-        text = "",
-        texthl = "DiagnosticSignError",
-        linehl = "",
-        numhl = "",
-      })
-
-      vim.fn.sign_define("DapBreakpointRejected", {
-        text = "", -- or "❌"
-        texthl = "DiagnosticSignError",
-        linehl = "",
-        numhl = "",
-      })
-
-      vim.fn.sign_define("DapStopped", {
-        text = "", -- or "→"
-        texthl = "DiagnosticSignWarn",
-        linehl = "Visual",
-        numhl = "DiagnosticSignWarn",
-      })
-
-      -- Automatically open/close DAP UI
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
+      -- Load mason-nvim-dap if available
+      if LazyVim.has("mason-nvim-dap.nvim") then
+        require("mason-nvim-dap").setup(LazyVim.opts("mason-nvim-dap.nvim"))
       end
 
-      local opts = { noremap = true, silent = true }
-      local map = vim.keymap.set
+      -- Set highlight for DapStoppedLine
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
-      -- Toggle breakpoint
-      map("n", "<leader>db", function()
-        dap.toggle_breakpoint()
-      end, opts)
+      -- Define signs for DAP
+      for name, sign in pairs(LazyVim.config.icons.dap) do
+        sign = type(sign) == "table" and sign or { sign }
+        vim.fn.sign_define(
+          "Dap" .. name,
+          { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
+        )
+      end
 
-      -- Continue / Start
-      map("n", "<leader>dc", function()
-        dap.continue()
-      end, opts)
+      -- Setup DAP configuration using VsCode launch.json file
+      local vscode = require("dap.ext.vscode")
+      local json = require("plenary.json")
+      vscode.json_decode = function(str)
+        return vim.json.decode(json.json_strip_comments(str))
+      end
 
-      -- Step Over
-      map("n", "<leader>do", function()
-        dap.step_over()
-      end, opts)
+      -- Load launch configurations from .vscode/launch.json if it exists
+      if vim.fn.filereadable(".vscode/launch.json") then
+        vscode.load_launchjs()
+      end
 
-      -- Step Into
-      map("n", "<leader>di", function()
-        dap.step_into()
-      end, opts)
+      -- Function to load environment variables
+      local function load_env_variables()
+        local variables = {}
+        for k, v in pairs(vim.fn.environ()) do
+          variables[k] = v
+        end
 
-      -- Step Out
-      map("n", "<leader>dO", function()
-        dap.step_out()
-      end, opts)
+        -- Load variables from .env file manually
+        local env_file_path = vim.fn.getcwd() .. "/.env"
+        local env_file = io.open(env_file_path, "r")
+        if env_file then
+          for line in env_file:lines() do
+            for key, value in string.gmatch(line, "([%w_]+)=([%w_]+)") do
+              variables[key] = value
+            end
+          end
+          env_file:close()
+        else
+          print("Error: .env file not found in " .. env_file_path)
+        end
+        return variables
+      end
 
-      -- Keymap to terminate debugging
-      map("n", "<leader>dq", function()
-        require("dap").terminate()
-      end, opts)
-
-      -- Toggle DAP UI
-      map("n", "<leader>du", function()
-        dapui.toggle()
-      end, opts)
+      -- Add the env property to each existing Go configuration
+      for _, config in pairs(dap.configurations.go or {}) do
+        config.env = load_env_variables
+      end
     end,
   },
   {
-    "mfussenegger/nvim-dap-python",
-    ft = "python",
+    "leoluz/nvim-dap-go",
+    ft = "go",
     dependencies = {
       "mfussenegger/nvim-dap",
-      "rcarriga/nvim-dap-ui",
     },
-    config = function(_, opts)
-      local path = "~/.local/share/nvim/mason/packages/debugpy/ven/bin/python"
-      require("dap-python").setup(path)
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-
-      -- dapui.setup()
-      -- dap.listeners.after.event_initialized["dapui_config"] = function()
-      --   dapui.open()
-      -- end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
+    -- keys = {
+    --   "<leader>dgt",
+    --   function()
+    --     require("dap-go").debug_test()
+    --   end,
+    --   desc = "Debug go test",
+    -- },
   },
 }
