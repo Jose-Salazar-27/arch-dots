e5cbc5b47786c06b1f0fe2290552634693ba903c
Jose-Salazar-27 <jose.dev2708@gmail.com>
2026-06-17 13:43:40 -0400

    update nvim indentation config

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,4 +1,7 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  enabled = false,
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {},
 }
--- .config/nvim/lua/plugins/mini.indentscope.lua
+++ .config/nvim/lua/plugins/mini.indentscope.lua
@@ -3,7 +3,7 @@ return {
     "nvim-mini/mini.indentscope",
     version = false, -- wait till new 0.7.0 release to put it back on semver
     event = "LazyFile",
-    enabled = false,
+    -- enabled = false,
 
     opts = {
       symbol = "|",
